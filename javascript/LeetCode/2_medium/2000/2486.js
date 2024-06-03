3c7405d8bcaf63afc6968ad31789fa40bf438935
ChoiRang <jkchldyd@gmail.com>
2024-06-03 11:03:45 +0900

    leetcode daily + 1

---
+++ javascript/LeetCode/2_medium/2000/2486.js
@@ -0,0 +1,18 @@
+/**Append Characters to String to Make Subsequence
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+const appendCharacters = function (s, t) {
+  let i = 0;
+  let j = 0;
+
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      j++;
+    }
+    i++;
+  }
+
+  return t.length - j;
+};
